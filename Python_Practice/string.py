7842a57642058b9828c9f36e43832301bd34de81
Rajasekhar Reddy Ankireddypalli <rajachandu4959@gmail.com>
2022-01-11 08:29:11 +0530

    added print lines inside struing reverse function

--- Python_Practice/string.py
+++ Python_Practice/string.py
@@ -1,3 +1,8 @@
 def str_rev(string):
-    return string[::-1]
-print(str_rev("hello....!"))
+    print("original string : ", string)
+    rev_string = string[::-1]
+    print("reversed string : ", rev_string)
+    return rev_string
+
+
+str_rev("hello...!")
